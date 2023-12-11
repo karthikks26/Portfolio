bb484ff06afbb9840ba07cff3d22bc2160b48ded
user pc <kskarthik728@gmail.com>
2023-12-11 23:45:47 +0530

    changes2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FormSubmiited from "./FormSubmiited.jsx";
-import Layout from "./layout.jsx";
+import Layout from "./Layout.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
