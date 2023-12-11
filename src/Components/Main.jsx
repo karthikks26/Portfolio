8bcff0086f1f33a17991be48e262be36bd31fca4
user pc <kskarthik728@gmail.com>
2023-12-12 00:01:50 +0530

    c1

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,7 +3,6 @@ import img from "../assets/photo.png";
 import { Sun, Moon } from "lucide-react";
 import { useContext } from "react";
 import { context } from "../MainLayout";
-import "animate.css/animate.css";
 
 const Main = () => {
   const { handleclick, state, setState } = useContext(context);
